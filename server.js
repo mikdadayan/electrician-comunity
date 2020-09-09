97bd50013d07b888a34366a849ef99c6cef70d03
mikdadayan <dadayan.mikayel@gmail.com>
2020-09-09 07:10:28 +0400

    Prepare for ...

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path')
 
 const connectDB = require('./config/db');
 
@@ -18,9 +19,12 @@ app.use((req, res, next) => {
 	next();
 });
 
-app.get('/', (req, res, next) => {
-	res.send('API RUNING');
-});
+if(process.env.NODE_ENV === "production"){
+	app.use(express.static('client/build'));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
 
 //define routes
 app.use('/api/auth', require('./routes/api/auth'));
